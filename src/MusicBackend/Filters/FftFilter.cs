f6084ab8bf2cf6733c80c11b5c80bc92fbfbc1a5
Paweł Oświeciński <wilsonek2001@gmail.com>
2024-03-25 17:38:47 +0100

    Change library used for fft and change filters a bit

--- src/MusicBackend/Filters/FftFilter.cs
+++ src/MusicBackend/Filters/FftFilter.cs
@@ -4,9 +4,37 @@ using MusicBackend.Model;
 namespace MusicBackend.Filters;
 public sealed class FftFilter : IFilter
 {
+	private readonly NWaves.Transforms.RealFft64 fft;
+	private readonly double[] re;
+	private readonly double[] im;
+	public FftFilter(int length)
+	{
+		fft = new(length);
+		//fftBuffer = new double[length];
+		re = new double[length];
+		im = new double[length];
+	}
 	public double[] process(double[] buffer)
 	{
-		var fftArray = FftSharp.FFT.Forward(buffer);
-		return FftSharp.FFT.Magnitude(fftArray,true);
+		fft.Direct(buffer,re,im);
+
+		
+		// normalize real and imaginary parts
+		//for (var i = 0; i < re.Length; i++)
+		//{
+		//	re[i] /= re.Length;
+		//	im[i] /= re.Length;
+		//}
+		
+		for (var i = 0; i < re.Length; i++)
+		{
+			buffer[i] = Math.Sqrt(re[i] * re[i] + im[i] * im[i]);
+		}
+		// normalize
+		for (var i = 0; i < buffer.Length; i++)
+		{
+			buffer[i] /= (buffer.Length);
+		}
+		return buffer;
 	}
 }
