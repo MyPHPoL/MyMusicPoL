341658da5e56f172a4d9a2bc89bd09a7a187ee46
piotrus <piotrradziszewski@outlook.com>
2024-06-10 15:38:48 +0200

    added a button to clear queue and changed topbar theme to dark

--- src/MyMusicPoL/Views/InputBoxView.xaml.cs
+++ src/MyMusicPoL/Views/InputBoxView.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,6 +10,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
@@ -19,6 +21,16 @@ namespace mymusicpol.Views;
 /// </summary>
 public partial class InputBoxView : Window
 {
+	[DllImport("DwmApi")] //System.Runtime.InteropServices
+	private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, int[] attrValue, int attrSize);
+
+	// change window topbar to dark theme
+	protected override void OnSourceInitialized(EventArgs e)
+	{
+		if (DwmSetWindowAttribute(new WindowInteropHelper(this).Handle, 19, new[] { 1 }, 4) != 0)
+			DwmSetWindowAttribute(new WindowInteropHelper(this).Handle, 20, new[] { 1 }, 4);
+	}
+
 	public string? TextBody { get; private set; } = "";
 	//public IRelayCommand ConfirmCommand = new RelayCommand(() => ConfirmHandler());
 	public InputBoxView(string labelText)
